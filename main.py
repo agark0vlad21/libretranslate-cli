ff0698a70d3e43c0362dd9498dfd98861eca7cfd
agark0vlad21 <72999843+agark0vlad21@users.noreply.github.com>
2022-12-12 07:31:14 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Simple CLI for LibreTranslate.org")
+parser.add_argument("-s", "--source", type=str, default="auto", help="source text language (default is auto)")
+parser.add_argument("-t", "--target", type=str, help="target language", required=True)
+args = parser.parse_args()
+
+import readline
+from requests import post
+
+while True:
+    try:
+        print(post("https://libretranslate.org/translate", json={
+    "q": input("type text here: "),
+    "source": args.source,
+    "target": args.target,
+    "format": "text",
+    "api_key": ""
+}, headers={
+    "Content-Type": "application/json"}).json()["translatedText"])
+    except KeyboardInterrupt:
+        print("\nBye!")
+        exit(1)
+    except KeyError:
+        print("can't get translation")
